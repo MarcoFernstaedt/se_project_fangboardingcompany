63e8d40c23a7a5e084da6a4cbcf476874cbc45f2
SpaceMan28 <nicokazemi34@gmail.com>
2023-08-12 20:34:35 -0600

    update form submit functionality

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -28,6 +28,7 @@ export default class PopupWithForm extends Popup {
     this._popupForm.addEventListener("submit", (evt) => {
       evt.preventDefault();
       this._handleFormSubmit(this._getInputValues());
+      this.close();
     });
   }
 }
